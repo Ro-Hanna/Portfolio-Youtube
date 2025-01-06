4755b1ef4f5bdc7ada9c7aaad224424c1a1cf64c
Ro-Hanna <54281655+Ro-Hanna@users.noreply.github.com>
2025-01-06 18:58:55 -0500

    fixed

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.jpg'
+import HeroImg2 from '../../images/HeroImage2.png'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
@@ -33,7 +33,7 @@ const HeroSection = () => {
 
                     <HeroRightContainer id="Right">
 
-                        <Img src={HeroImg} alt="hero-image" />
+                        <Img src={HeroImg2} alt="hero-image" />
                     </HeroRightContainer>
                 </HeroInnerContainer>
 
